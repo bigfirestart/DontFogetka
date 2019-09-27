fa4fbe35815d5f406133c212de169292504ed7c5
Kirill <bigfirestart@gmail.com>
2019-09-27 19:40:41 +0300

    idea-

---
+++ save.py
@@ -0,0 +1,9 @@
+from trello import TrelloClient
+import config
+
+client = Trecon,
+    token='4c9eddb81a000b672d3223a29426e925eeebba315b15b2dad9a65bda95a9b1c3',
+)
+def save():
+    all_boards = client.list_boards()
+    print(all_boards)
