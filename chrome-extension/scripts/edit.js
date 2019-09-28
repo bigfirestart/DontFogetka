3e81c5d7596a796e0a29b7e4ea15e1ecfd7562dc
Nikolay Rulev <nikolayr2001@gmail.com>
2019-09-28 20:39:41 +0300

    stop writing edit page

--- chrome-extension/scripts/edit.js
+++ chrome-extension/scripts/edit.js
@@ -11,7 +11,6 @@ function startVue() {
 	    flight: null,
 	    new_reason : "",
 	    reasons: [],
-	  	people_count: 1,
 	  	people: [
 	  		{
 	  			name: "",
@@ -23,45 +22,77 @@ function startVue() {
 	  },
 	  methods: {
 	  	addReason() {
-	  		if(this.reasons.indexOf(this.new_reason) === -1) {
+	  		if(this.reasons.indexOf(this.new_reason) === -1 && this.new_reason !== "") {
 	  			this.reasons.push(this.new_reason);
+	  			this.new_reason = "";
 	  		}
 	  	},
+		addHuman() {
+	  		this.people.push({
+	  			name: "",
+	  			adult: true,
+	  			sex: "Муж"
+	  		});
+	  	},
+		deleteHuman(index) {
+	  		this.people.splice(index, 1);
+		},
 	  	offer() {
+	  		let ok = true;
 	  		if(this.reasons.length < 1) {
 	  			this.$buefy.notification.open({
-            duration: 3000,
-            message: `Пожалуйста, укажите ваши цели`,
-            position: 'is-bottom-right',
-            type: 'is-info',
-            hasIcon: true
-	        })
+					duration: 3000,
+					message: `Пожалуйста, укажите ваши цели`,
+					position: 'is-bottom-right',
+					type: 'is-info',
+					hasIcon: true
+				});
+	  			ok = false;
 	  		}
 	  		for(let i = 0; i < this.reasons.length; i += 1) {
 	  			if(this.reasons[i].name === "") {
 	  				this.$buefy.notification.open({
-	            duration: 3000,
-	            message: `Пожалуйста, заполните имя путешественника №${i + 1}`,
-	            position: 'is-bottom-right',
-	            type: 'is-info',
-	            hasIcon: true
-	        	})
+						duration: 3000,
+						message: `Пожалуйста, заполните имя путешественника №${i + 1}`,
+						position: 'is-bottom-right',
+						type: 'is-info',
+						hasIcon: true
+					});
+	  				ok = false;
 	  			}
 	  		}
+	  		if(ok === true) {
+	  			for(let i = 0; i < this.people.length; i += 1) {
+	  				if(this.people[i].sex === "Муж") {
+	  					this.people[i].sex = "M";
+					} else {
+	  					this.people[i].sex = "F";
+					}
+				}
+	  			let requestInfo = {
+					...this.flight,
+					travel_type: this.reasons,
+					people: {
+						count: this.people.length,
+						tourists: this.people
+					}
+	  			};
+	  			console.log(requestInfo);
+	  			chrome.storage.sync.set({
+					requestInfo
+        		});
+			}
 	  	}
 	  },
 	  watch: {
 	  	people_count(new_val, old_val) {
-	  		if(new_val > old_val) {
-		  		this.people.push({
-		  			name: "",
-		  			adult: true,
-		  			sex: "Муж"
-		  		})
-	  		} else {
-	  			this.people.pop()
-	  		}
+	  		console.log("people count changed", old_val, new_val);
 	  	}
+	  },
+	  computed: {
+	  	people_count() {
+	  		return this.people.length;
+		},
 	  },
 	  beforeCreate() {
 			chrome.storage.sync.get("flights", res => {
