8cb0062b4be9b1c9ff458a68272009a3a52d054e
Nikolay Rulev <nikolayr2001@gmail.com>
2019-09-28 08:44:20 +0300

    refactoring

--- build.py
+++ build.py
@@ -1,59 +1,71 @@
 import json
+from pprint import pprint
 
 
-def isInRange(range: str, temp: int):
-    max = int(range.split()[0])
-    min = int(range.split()[1])
-    if temp >= min and temp <= max:
-        return 1
+def is_in_range(temperature_range: str, temperature: int):
+    max_temp = int(temperature_range.split()[0])
+    min_temp = int(temperature_range.split()[1])
+    if min_temp <= temperature <= max_temp:
+        return True
     else:
-        return 0
+        return False
 
 
-Seasons = {0: "winter", 1: "spring", 2: "summer", 3: "fall", 4: "winter"}
+def get_season_by_month(month: int):
+    if month == 12 or month <= 2:
+        return "winter"
+    elif 3 <= month <= 5:
+        return "spring"
+    elif 6 <= month <= 8:
+        return "summer"
+    elif 9 <= month <= 11:
+        return "fall"
+    else:
+        raise ValueError
 
 
-def build(list):
+def build(request):
     # TODO ищет месяц
-    city = list["destination_point"]
-    month = int(int(list["arrival_date"].split(".")[1]) / 3)
-    month = Seasons[month]
+    city = request["destination_point"]
+    month = int(int(request["arrival_date"].split(".")[1]) / 3)
+    month = get_season_by_month(month)
     # TODO смотрит компанию
     males_count = 0
     females_count = 0
     adults = 0
-    for el in list["people"]["tourists"]:
-        if not el["adult"]:
+    for tourist in request["people"]["tourists"]:
+        if not tourist["adult"]:
             adults = adults + 1
-        if el["sex"] == "male":
+        if tourist["sex"] == "male":
             males_count = males_count + 1
-        if el["sex"] == "female":
+        if tourist["sex"] == "female":
             females_count = females_count + 1
 
     # TODO смотрит погоду в данном регионе по месяцу
-    weather_list = json.loads(open("templates/Weather.json", 'r').read())
-    for el in weather_list:
-        if el == city:
-            weather = int(weather_list[el][month])
+    weather_list = json.load(open("templates/Weather.json"))
+    for tourist in weather_list:
+        if tourist == city:
+            weather = int(weather_list[tourist][month])
+
     # TODO подбирает вещи
     clothes_list = json.loads(open("templates/Clothes.json", 'r').read())
     final_list = json.loads(open("templates/EmptyPrediction.json", 'r').read())
-    for el in clothes_list:
-        if isInRange(clothes_list[el]["temp"], weather):
-            if clothes_list[el]["sex"] == '':
-                name = el
+    for tourist in clothes_list:
+        if is_in_range(clothes_list[tourist]["temp"], weather):
+            if clothes_list[tourist]["sex"] == '':
+                name = tourist
                 count = males_count + females_count
-            if clothes_list[el]["sex"] == 'M' and males_count > 0:
-                name = el
+            if clothes_list[tourist]["sex"] == 'M' and males_count > 0:
+                name = tourist
                 count = males_count
-            if clothes_list[el]["sex"] == 'F' and females_count > 0:
-                name = el
+            if clothes_list[tourist]["sex"] == 'F' and females_count > 0:
+                name = tourist
                 count = females_count
-            final_list["clothes"][clothes_list[el]["type"]].append({"name": name, "count": count})
+            final_list["clothes"][clothes_list[tourist]["type"]].append({"name": name, "count": count})
     # TODO добавляем активити
     activity_list = json.loads(open("templates/Activities.json", 'r').read())
     name = ""
-    for actv_name in list["travel_type"]:
+    for actv_name in request["travel_type"]:
         for actv in activity_list:
             if activity_list[actv]["type"] == actv_name:
                 if activity_list[actv]["sex"] == '':
@@ -77,5 +89,33 @@ def build(list):
 
     # TODO добавляем погоду
     final_list["advices"]["info"] = {"temperature": weather}
-    final_list["source"] = list
+    final_list["source"] = request
     return final_list
+
+
+if __name__ == '__main__':
+    req = {
+        "departure_point": "Moscow",
+        "destination_point": "Zurich",
+        "arrival_date": "23.12.2019",
+        "return_date": "01.10.2019",
+        "travel_type": ["sking", "work"],
+        "people": {
+            "count": 3,
+            "tourists": [
+                {
+                    "sex": "male",
+                    "adult": True
+                },
+                {
+                    "sex": "female",
+                    "adult": True
+                },
+                {
+                    "sex": "male",
+                    "adult": False
+                }
+            ]
+        }
+    }
+    pprint(build(req))
--- web.py
+++ web.py
@@ -1,17 +1,21 @@
 from flask import Flask, request, Response
 import json
-import save , build
+import save, build
+
 app = Flask(__name__)
 
+
 @app.route('/build', methods=['POST'])
-def buildList():
-    res= build.build(request.json)
+def build_predictions():
+    res = build.build(request.json)
     return Response(json.dumps(res, ensure_ascii=False), mimetype='application/json')
 
+
 @app.route('/save', methods=['POST'])
-def saveList():
+def save_predictions():
     response = save.save(request.json)
     return "ok"
+
 
 if __name__ == '__main__':
     app.run()
