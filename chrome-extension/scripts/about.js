f925fb920df4444982d4e4f510eb0fac98ff66ae
unknown <godis7.15@gmail.com>
2019-09-28 04:56:56 +0300

    Removing flights via popup menu

--- chrome-extension/scripts/about.js
+++ chrome-extension/scripts/about.js
@@ -3,16 +3,53 @@ $(document).ready(function() {
 });
 
 function renderSavedFlights() {
+
+    var $flightsList = $('#flights-list');
+    $flightsList.children().remove();
+
     chrome.storage.sync.get('flights', res => {
         var infoArr = res.flights.arr;
         if (!infoArr) return;
-        
-        var $flightsList = $('#flights-list');
 
         for(var i = 0; i < infoArr.length; ++i) {
-            var $item = $('<div>').text('From: ' + infoArr[i].departure_point);
+            var $item = $('<div>')
+                .text('From: ' + infoArr[i].departure_point);
+
+            var $setupRef = $('<a>')
+                .attr('href', '#')
+                .text('set up')
+                .appendTo($item);
+
+            var $removeBtn = $('<button>')
+                .click(ev => {
+                    var $target = $(ev.originalEvent.target).parent();
+                    var index = $flightsList.children().index($target);
+                    removeFlight(index, () => {
+                        renderSavedFlights();
+                    });
+                })
+                .text('remove')
+                .appendTo($item);
+
+            
             $flightsList.append($item);
         }
     });
 }
 
+function removeFlight(index, refreshCallback) {
+    chrome.storage.sync.get('flights', res => {
+        var infoArr = res.flights.arr;
+        if (!infoArr) return;
+
+        infoArr = infoArr.filter((_, i) => i !== index);
+
+        console.log( JSON.stringify(infoArr) );
+
+        chrome.storage.sync.set({
+            flights: {
+                arr: infoArr
+            }
+        }, refreshCallback);
+    });
+}
