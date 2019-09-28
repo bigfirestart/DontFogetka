1746c68134ba7dedd3ad3f7c30e6b200084a2a4e
Nikolay Rulev <nikolayr2001@gmail.com>
2019-09-28 09:28:39 +0300

    refactoring

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import json
 from pprint import pprint
 
 
-def is_in_range(temperature_range: str, temperature: int):
+def is_in_temperature_range(temperature_range: str, temperature: int):
     max_temp = int(temperature_range.split()[0])
     min_temp = int(temperature_range.split()[1])
     if min_temp <= temperature <= max_temp:
@@ -24,73 +24,97 @@ def get_season_by_month(month: int):
         raise ValueError
 
 
+def get_count_by_sex(sex, **kwargs):
+    count = 0
+    if sex == '':
+        count = kwargs["males_count"] + kwargs["females_count"]
+    elif sex == 'M':
+        count = kwargs["males_count"]
+    elif sex == 'F':
+        count = kwargs["females_count"]
+    return count
+
+
+def add_clothes(tourists, available, **kwargs):
+    result = []
+    temperature = kwargs["temperature"]
+    males_count = kwargs["males_count"]
+    females_count = kwargs["females_count"]
+    for item in available:
+        if is_in_temperature_range(available[item]["temp"], temperature):
+            count = get_count_by_sex(available[item]["sex"], **kwargs)
+            if count > 0:
+                result[available[item]["type"]].append({"name": item, "count": count})
+    return result
+
+
+def add_activities(activity, available, **kwargs):
+    males_count = kwargs["males_count"]
+    females_count = kwargs["females_count"]
+    result = []
+    for item in available:
+        if available[item] == activity:
+            count = get_count_by_sex(available[item]["sex"], **kwargs)
+            result.append({"name": item, "count": count})
+
+
 def build(request):
     # TODO ищет месяц
     city = request["destination_point"]
-    month = int(int(request["arrival_date"].split(".")[1]) / 3)
-    month = get_season_by_month(month)
+    # 12.12.2109
+    month = get_season_by_month(int(request["arrival_date"].split(".")[1]))
     # TODO смотрит компанию
     males_count = 0
     females_count = 0
-    adults = 0
+    children = 0
     for tourist in request["people"]["tourists"]:
         if not tourist["adult"]:
-            adults = adults + 1
+            children += 1
         if tourist["sex"] == "male":
-            males_count = males_count + 1
-        if tourist["sex"] == "female":
-            females_count = females_count + 1
+            males_count += 1
+        elif tourist["sex"] == "female":
+            females_count += 1
 
     # TODO смотрит погоду в данном регионе по месяцу
     weather_list = json.load(open("templates/Weather.json"))
-    for tourist in weather_list:
-        if tourist == city:
-            weather = int(weather_list[tourist][month])
+    temperature = weather_list[request["destination_point"]][month]
 
     # TODO подбирает вещи
-    clothes_list = json.loads(open("templates/Clothes.json", 'r').read())
-    final_list = json.loads(open("templates/EmptyPrediction.json", 'r').read())
-    for tourist in clothes_list:
-        if is_in_range(clothes_list[tourist]["temp"], weather):
-            if clothes_list[tourist]["sex"] == '':
-                name = tourist
-                count = males_count + females_count
-            if clothes_list[tourist]["sex"] == 'M' and males_count > 0:
-                name = tourist
-                count = males_count
-            if clothes_list[tourist]["sex"] == 'F' and females_count > 0:
-                name = tourist
-                count = females_count
-            final_list["clothes"][clothes_list[tourist]["type"]].append({"name": name, "count": count})
+    clothes_list = json.load(open("templates/Clothes.json"))
+    result = json.load(open("templates/EmptyPrediction.json"))
+
+    result["clothes"].update(
+        add_clothes(
+            request["people"]["tourists"],
+            clothes_list,
+            temperature=temperature,
+            males_count=males_count,
+            females_count=females_count
+        )
+    )
+
     # TODO добавляем активити
     activity_list = json.loads(open("templates/Activities.json", 'r').read())
-    name = ""
-    for actv_name in request["travel_type"]:
-        for actv in activity_list:
-            if activity_list[actv]["type"] == actv_name:
-                if activity_list[actv]["sex"] == '':
-                    name = actv
-                    count = males_count + females_count
-                if activity_list[actv]["sex"] == 'M' and males_count > 0:
-                    name = actv
-                    count = males_count
-                if activity_list[actv]["sex"] == 'F' and females_count > 0:
-                    name = actv
-                    count = females_count
-                final_list["activities"].append({"name": name, "count": count})
+    for travel_activity in request["travel_type"]:
+        result["activities"] += add_activities(
+            travel_activity, activity_list,
+            males_count=males_count,
+            females_count=females_count
+        )
+
     # TODO добавляем гигиену костылём
-    final_list["hygiene/cosmetics"].append({"name": "Зубная щётка", "count": males_count + females_count})
-    final_list["hygiene/cosmetics"].append({"name": "Зубная паста", "count": 1})
-    final_list["hygiene/cosmetics"].append(
+    result["hygiene/cosmetics"].append({"name": "Зубная щётка", "count": males_count + females_count})
+    result["hygiene/cosmetics"].append({"name": "Зубная паста", "count": 1})
+    result["hygiene/cosmetics"].append(
         {"name": "Гель для душа", "count": int(males_count > 0) + int(females_count > 0)})
-    final_list["hygiene/cosmetics"].append({"name": "Шампунь", "count": int(males_count > 0) + int(females_count > 0)})
-    final_list["hygiene/cosmetics"].append({"name": "Мачалка", "count": males_count + females_count})
-    final_list["hygiene/cosmetics"].append({"name": "Косметичка", "count": females_count})
+    result["hygiene/cosmetics"].append({"name": "Шампунь", "count": int(males_count > 0) + int(females_count > 0)})
+    result["hygiene/cosmetics"].append({"name": "Мачалка", "count": males_count + females_count})
+    result["hygiene/cosmetics"].append({"name": "Косметичка", "count": females_count})
 
     # TODO добавляем погоду
-    final_list["advices"]["info"] = {"temperature": weather}
-    final_list["source"] = request
-    return final_list
+    result["advices"]["info"] = {"temperature": weather}
+    result["source"] = request
+    return result
 
 
 if __name__ == '__main__':
