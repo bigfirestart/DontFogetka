d06f45a4ba1d0a340ceff6e241805772633abd82
unknown <godis7.15@gmail.com>
2019-09-29 11:52:25 +0300

    Almost last commit

--- chrome-extension/scripts/about.js
+++ chrome-extension/scripts/about.js
@@ -64,47 +64,52 @@ function renderSavedFlights() {
         }
 
         for(var i = 0; i < infoArr.length; ++i) {
-            
+
             var $item = $('<div>')
                 .addClass('flight-item both');
+
+            var $leftCol = $('<div>').appendTo($item);
+            var $rightCol = $('<div>').appendTo($item);
 
             var $depart = $('<div>')
                 .text(infoArr[i].departure_point)
                 .addClass('depart')
-                .appendTo($item);
+                .appendTo($leftCol);
 
             var $destin = $('<div>')
                 .text(infoArr[i].destination_point)
                 .addClass('destin')
-                .appendTo($item);
+                .appendTo($leftCol);
 
             var $date_to = $('<div>')
                 .text(infoArr[i].arrival_date)
                 .addClass('date-to')
-                .appendTo($item);
+                .appendTo($leftCol);
 
             if (infoArr[i].return_date)
                 var $date_from = $('<div>')
                     .text(infoArr[i].return_date)
                     .addClass('date-from')
-                    .appendTo($item);
+                    .appendTo($leftCol);
 
-            var $setupRef = $('<button>')
-                .text('set up')
+            var $setupRef = $('<div>')
+                .addClass('btn')
+                .text('запланировать')
                 .click( createClickListener(i) )
-                .appendTo($item);
+                .appendTo($rightCol);
 
-            var $removeBtn = $('<button>')
+            var $removeBtn = $('<div>')
+                .addClass('btn')
                 .click(ev => {
-                    var $target = $(ev.originalEvent.target).parent();
+                    var $target = $(ev.originalEvent.target).parent().parent();
                     var index = $flightsList.children().index($target);
                     removeFlight(index, () => {
                         renderSavedFlights();
                     });
                 })
-                .text('x')
+                .text('убрать')
                 .addClass('remove-btn')
-                .appendTo($item);
+                .appendTo($rightCol);
 
             $flightsList.append($item);
         }
@@ -162,21 +167,26 @@ function sendTripInfo() {
         destination_point: $('#destin-info').text(),
         arrival_date: $('#date-to-info').text(),
         return_date: $('#date-from-info').text(),
-
+        travel_type: [],
         people: {
-            tourits: []
+            tourists: []
         }
     };
     
-    var $passangers = $('#pass-container');
+    var $tags = $('input[name=tag]:checked');
+    for(var i = 0; i < $tags.length; ++i) {
+        flight.travel_type.push( $tags.eq(i).val() );
+    }
+
+    var $passangers = $('#pass-container').children();
     flight.people.count = $passangers.length;
 
     for (var i = 0; i < flight.people.count; ++i) {
         var $pass = $passangers.eq(i);
-        flight.people.tourits.push({
-            name: $pass.find('#name-id').val(),
-            sex: $pass.find('radio[name=sex]:checked').val(),
-            age: $pass.find('radio[name=age]:checked').val() === 'adult',
+        flight.people.tourists.push({
+            name: $pass.find('#pass-name-id').val(),
+            sex: $pass.find('input[name=sex]:checked').val(),
+            adult: $pass.find('input[name=age]:checked').val() === 'adult',
         });
     }
 
@@ -185,7 +195,12 @@ function sendTripInfo() {
     var uri = 'http://95.216.157.100:5000/';
     $.post(uri + 'build', JSON.stringify(flight))
     .done(res => {
-        $.post(uri + 'save', JSON.stringify(res))
+        res.source = flight;
+        var resJSON = JSON.stringify(res);
+        console.log(resJSON);
+
+        $.post(uri + 'save', resJSON)
         .done(() => console.log('Visit trello'));
-    });
+    })
+    .fail(err => console.error(err));
 }
